7c70d50fc0868b8c30cfa2a408493dc88579d22b
gren8 <greg.renfro@gmail.com>
2021-10-15 19:48:43 -0700

    added goroutines to find player info

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -12,7 +13,7 @@ func main() {
 	//helps benchmarking the request time
 	now := time.Now()
 
-	rosterFile, err := os.OpenFile("rosters.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	rosterFile, err := os.OpenFile("rosters.txt", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatalf("error opening the file rosters.txt: %v", err)
 	}
@@ -28,12 +29,48 @@ func main() {
 		log.Fatalf("error while getting all teams: %v", err)
 	}
 
+	wg := sync.WaitGroup{}
+	wg.Add(len(teams))
+
+	//unbuffered channel
+	result := make(chan []nhlAPI.Roster)
+
 	for _, team := range teams {
-		log.Println("-------------------------------------")
-		log.Printf("Name: %s", team.Name)
-		log.Println("-------------------------------------")
+		go func(team nhlAPI.Team) {
+			roster, err := nhlAPI.GetRosters(team.ID)
+			if err != nil {
+				log.Fatalf("error getting roster: %v", err)
+
+			}
+
+			result <- roster
+
+			wg.Done()
+		}(team)
 
 	}
 
+	go func() {
+		wg.Wait()
+		close(result)
+
+	}()
+
+	display(result)
+
 	log.Printf("took %v", time.Now().Sub(now).String())
+}
+
+func display(results chan []nhlAPI.Roster) {
+	for r := range results {
+		for _, ros := range r {
+			log.Println("----------------------------")
+			log.Printf(" Name: %s\n", ros.Person.FullName)
+			log.Printf(" ID: %d\n", ros.Person.ID)
+			log.Printf(" Position: %s\n", ros.Position.Abbreviation)
+			log.Printf(" Jersey: %s\n", ros.JerseyNumber)
+			log.Println("----------------------------")
+
+		}
+	}
 }
